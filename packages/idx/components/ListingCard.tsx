7339e266ab21d473566b92abb82d1ab41b9f8751
Patrick Aladadyan <tenurecapitalinvestments@gmail.com>
2025-07-26 23:10:57 -0700

    refactor: remove unused React import from ListingCard

--- packages/idx/components/ListingCard.tsx
+++ packages/idx/components/ListingCard.tsx
@@ -1,1 +1,28 @@
-export const ListingCard=()=>null;
+// packages/idx/components/ListingCard.tsx
+
+
+export interface Listing {
+  id: string;
+  slug: string;
+  address: string;
+  city: string;
+  state: string;
+  price: number;
+  beds: number;
+  baths: number;
+  sqft: number;
+  photoUrl: string;
+}
+
+export const ListingCard: React.FC<{ listing: Listing }> = ({ listing }) => (
+  <article style={{ border: '1px solid #ddd', padding: 12, borderRadius: 8 }}>
+    <h3>{listing.address}</h3>
+    <p>
+      {listing.city}, {listing.state}
+    </p>
+    <p>
+      ${listing.price.toLocaleString()} &middot; {listing.beds} bd&nbsp;|&nbsp;
+      {listing.baths} ba&nbsp;|&nbsp;{listing.sqft.toLocaleString()} sq ft
+    </p>
+  </article>
+);
